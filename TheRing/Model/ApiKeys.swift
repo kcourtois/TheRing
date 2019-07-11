eb0771b61b7ebb32051d9583131824624abc16b4
Kévin Courtois <22455158+kcourtois@users.noreply.github.com>
2019-07-11 12:45:48 +0200

    Create ApiKeys.swift
    Add this file for travis CI tests

---
+++ TheRing/Model/ApiKeys.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKeys.swift
+//  TheRing
+//
+//  Created by Kévin Courtois on 03/07/2019.
+//  Copyright © 2019 Kévin Courtois. All rights reserved.
+//
+
+import Foundation
+
+class ApiKeys {
+    static let tmdbKey: String = "TMDB_API_KEY_HERE"
+}
